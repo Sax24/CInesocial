4195705d66abf00747baf59d6992c1cf45c14328
Fy <fyramandimbison@gmail.com>
2026-05-22 13:58:20 +0200

    ajout url front-end vercel

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -1,17 +1,29 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"strings"
+)
 
 func CORSMiddleware(next http.Handler) http.Handler {
+	frontendURL := os.Getenv("FRONTEND_URL")
+
+	allowedOrigins := map[string]bool{
+		"http://localhost:5173": true,
+		frontendURL:            true,
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 
-		if origin == "http://localhost:5173" {
+		if allowedOrigins[origin] {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Vary", "Origin")
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		}
+
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
